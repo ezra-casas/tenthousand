d35a420ef21f940b6d54b862c5a4f59c1909fef2
azhorabai <97122016+ezra-casas@users.noreply.github.com>
2023-08-30 12:38:28 -0700

    - initial commit

---
+++ src/components/Login/Login.tsx
@@ -0,0 +1,13 @@
+export default function Login(){
+    return (
+        <div>
+            <section>
+                <div>
+
+                </div>
+            </section>
+            <section>Mid</section>
+            <section>Bot</section>
+        </div>
+    );
+}
